0dd0bd344defd3bfda97d01a4c2fbd868b6e7527
Isaac Hess <isaachess@gmail.com>
2014-12-12 09:31:30 -0700

    Koa app working on blog server

--- multiple-services/blog-service/blog-server.js
+++ multiple-services/blog-service/blog-server.js
@@ -1,11 +1,13 @@
-var express = require('express')
+var koa = require('koa')
+var router = require('koa-router')
 var r = require('rethinkdb')
 var config = require('../common/config')
 
-var app = express()
+var app = koa()
+app.use(router(app))
 
-app.get('/posts/:id', function (req, res) {
-    res.send('Hello World from the blog server!')
+app.get('/posts/:id', function* (next) {
+    this.body = 'Hello World from the blog server!'
 })
 
 app.listen(3001)
--- multiple-services/main-api/main-server.js
+++ multiple-services/main-api/main-server.js
@@ -1,4 +1,3 @@
-var express = require('express')
 var koa = require('koa')
 var router = require('koa-router')
 var request = require('request')
